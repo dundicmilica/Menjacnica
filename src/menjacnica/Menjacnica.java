e2f69de4f0e4af4a87868b1f8fe6958cabe967eb
dundicmilica <dundic.milica94@gmail.com>
2015-03-30 19:42:11 +0200

    Postavljeno da klasa Menjacnica implementira interfejs MenjacnicaInterfejs. Generisane (za sada nefunkcionalne) metode iz interfejsa.

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,8 +1,27 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import menjacnicainterfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
 	
 	public LinkedList<Valuta> valuta = new LinkedList<Valuta>();
+
+	public void dodajKurs(double prodajniKurs, double kupovniKurs,
+			GregorianCalendar dan, String naziv, String skraceniNaziv) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void brisanjeKursa(GregorianCalendar dan, String naziv) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Valuta pronadjiKursNaDan(GregorianCalendar dan, String naziv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
