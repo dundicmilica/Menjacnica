a38607f7a3d0bdcfeb4fac3d738a20f719511574
dundicmilica <dundic.milica94@gmail.com>
2015-03-30 19:52:59 +0200

    Implementirane metode 	dodajKurs 	brisanjeKursa 	prnadjiKursNaDan

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -11,17 +11,46 @@ public class Menjacnica implements MenjacnicaInterfejs{
 
 	public void dodajKurs(double prodajniKurs, double kupovniKurs,
 			GregorianCalendar dan, String naziv, String skraceniNaziv) {
-		// TODO Auto-generated method stub
 		
+		Valuta v = new Valuta();
+		
+		v.setDatum(dan);
+		v.setKupovniKurs(kupovniKurs);
+		v.setProdajniKurs(prodajniKurs);
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		v.setSrednjiKurs((prodajniKurs + kupovniKurs) / 2);
+		
+		valuta.add(v);
 	}
 
 	public void brisanjeKursa(GregorianCalendar dan, String naziv) {
-		// TODO Auto-generated method stub
 		
+		Valuta v = new Valuta();
+		
+		v.setDatum(dan);
+		v.setNaziv(naziv);
+		
+		for (int i = 0; i < valuta.size(); i++) {
+			if (valuta.get(i).equals(v))
+				valuta.remove(i);
+		}
 	}
 
 	public Valuta pronadjiKursNaDan(GregorianCalendar dan, String naziv) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Valuta v = new Valuta();
+		Valuta pomValuta = null;
+		
+		v.setDatum(dan);
+		v.setNaziv(naziv);
+		
+		for (int i = 0; i < valuta.size(); i++) {
+			if (valuta.get(i).equals(v)) {
+				pomValuta = v;
+				break;
+			}
+		}
+		return pomValuta;
 	}
 }
